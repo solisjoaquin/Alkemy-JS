f1764d0df3fef534a88ba159d3030b63a1f2e2c7
Joaquin <sol19008@byui.edu>
2021-02-11 13:32:12 -0300

    adding routes with react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import './App.css'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
 
 const App = () => {
 
   // State for retrieving player ID numbers from roster endpoint
   const [currenntBalance, setCurrentBalance] = useState(null)
   const [transactions, setTransactions] = useState([]);
-
+  const [editTransaction, setEditTransaction] = useState([])
   useEffect(() => {
     axios.get(`http://localhost:5000/transactions`)
       .then(
@@ -23,18 +30,98 @@ const App = () => {
 
 
   return (
-    <div className="App-header">
+    <Router>
+      <div className="App-header">
+        <nav>
+          <ul>
+            <li>
+              <Link className="links" to="/">Home</Link>
+            </li>
+            <li>
+              <Link className="links" to="/create">New</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/create">
+            <CreateTransaction />
+          </Route>
+          <Route path="/edit/:id">
+            <EditTransaction transactions={transactions} />
+          </Route>
+          <Route exact path="/">
+            <List transactions={transactions} currenntBalance={currenntBalance} />
+          </Route>
+        </Switch>
+
+      </div>
+    </Router>
+  )
+}
+
+const List = ({ transactions, currenntBalance }) => {
+  return (
+    <div>
       <h1>Current balance: {currenntBalance}</h1>
-      { transactions.map((transaction) =>
+
+      {transactions.map((transaction) =>
         <div className="transaction-li">
           <li>{`
-        Concept = ${transaction.concept} - 
-        Amount = ${transaction.amount} - 
-        Type = ${transaction.type}`}
+  Concept = ${transaction.concept} - 
+  Amount = ${transaction.amount} - 
+  Type = ${transaction.type}`}
           </li>
-          <button className=" button-edit">Edit</button>
+
+          <button className=" button-edit">
+            <Link to={`/edit/${transaction.id}`}>Edit</Link>
+          </button>
         </div>
       )}
+    </div>
+  )
+}
+
+const EditTransaction = ({ transactions }) => {
+  const { id } = useParams()
+  return (
+    <div>
+      <h1>Edit transaction</h1>
+      {transactions.filter(transaction => transaction.id == id).map((transaction) =>
+        <div className="">
+          <form action="">
+            <label for="fname">Concept</label><br />
+            <input type="text" id="fname" name="fname" value={transaction.concept} /><br />
+            <label for="fname">Amount</label><br />
+            <input type="text" id="fname" name="fname" value={transaction.amount} /><br />
+            <label for="fname">Date</label><br />
+            <input type="text" id="fname" name="fname" value={transaction.date} /><br />
+            <input type="submit" />
+          </form>
+        </div>
+      )}
+    </div>
+  )
+}
+
+const CreateTransaction = () => {
+  return (
+    <div>
+      <h1>Create transaction</h1>
+      <div className="">
+        <form action="">
+          <label for="fname">Concept</label><br />
+          <input type="text" id="fname" name="fname" /><br />
+          <label for="fname">Amount</label><br />
+          <input type="text" id="fname" name="fname" /><br />
+          <label for="fname">Date</label><br />
+          <input type="text" id="fname" name="fname" /><br />
+          <input type="radio" id="male" name="gender" value="male" />
+          <label for="male">income</label><br />
+          <input type="radio" id="female" name="gender" value="female" />
+          <label for="female">Outcome</label><br />
+          <input type="submit" />
+        </form>
+      </div>
     </div>
   )
 }
